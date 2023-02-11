7a7f3ac4e2c0fe3de59b8f97e733663774ee963c
joserubio16 <jose.rubio16@hotmail.com>
2023-02-11 13:04:05 -0600

    tipado estatico

--- build/index.js
+++ build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hola');
+let text = 'hola';
+text = 'hola2';
+console.log(text);
+function suma(num1, num2) {
+    return num1 + num2;
+}
+console.log(suma(1, 3));
+let a = '';
+let booleanos = false;
+let arreglos = ['hola', a, text];
+let objetos = { id: 1, nombre: 'Pedro' };
+// null no es lo mismo que void
+let nulos = null;
+function imprimirEnConsola(mensaje) {
+    console.log(mensaje);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('hola')
+let text: string = 'hola'
+text = 'hola2'
+console.log(text)
+
+function suma(num1: number, num2: number): number{
+    return num1 + num2
+}
+
+console.log(suma(1,3))
+
+let a:string=''
+
+let booleanos: boolean = false
+let arreglos: string[] = ['hola', a, text]
+let objetos: object = {id: 1, nombre: 'Pedro'}
+
+// null no es lo mismo que void
+let nulos: null = null
+function imprimirEnConsola (mensaje: string): void{
+    console.log(mensaje)
+}
