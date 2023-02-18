5e22e70783e298d8af7aa47100f4c2a7c0ec8f03
joserubio16 <jose.rubio16@hotmail.com>
2023-02-18 13:16:15 -0600

    expresiones

---
+++ build/src/session-2/expresiones.js
@@ -0,0 +1,6 @@
+"use strict";
+const value = 10;
+const message = (value > 10) ?
+    'El valor es mayor que 10' :
+    'El valor es menor o igual a 10';
+console.log(message);
---
+++ src/session-2/expresiones.ts
@@ -0,0 +1,7 @@
+const value : number = 10
+
+const message: string = (value > 10) ?
+ 'El valor es mayor que 10' :
+  'El valor es menor o igual a 10'
+
+console.log(message)
