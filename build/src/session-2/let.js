15f04249fd7e32d86ea74d080c316707a86d5c20
joserubio16 <jose.rubio16@hotmail.com>
2023-02-18 11:22:15 -0600

    variables let

---
+++ build/src/session-2/let.js
@@ -0,0 +1,13 @@
+"use strict";
+var index = 0;
+if (index === 0) {
+    var index = 2;
+    console.log(`index = ${index}`);
+}
+console.log(`index = ${index}`);
+let indexlet = 0;
+if (indexlet === 0) {
+    let indexlet = 2;
+    console.log(`indexlet = ${indexlet}`);
+}
+console.log(`indexlet = ${indexlet}`);
---
+++ src/session-2/let.ts
@@ -0,0 +1,17 @@
+var index: number = 0
+
+if(index === 0){
+    var index = 2
+    console.log(`index = ${index}`)
+}
+
+console.log(`index = ${index}`)
+
+let indexlet: number = 0
+
+if(indexlet === 0){
+    let indexlet = 2
+    console.log(`indexlet = ${indexlet}`)
+}
+
+console.log(`indexlet = ${indexlet}`)
