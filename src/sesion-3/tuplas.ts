36aab8c75361e928cf145d89c31edbbd1c7baf28
joserubio16 <jose.rubio16@hotmail.com>
2023-02-25 11:36:40 -0600

    desestructuracion de tuplas y objetos

---
+++ src/sesion-3/tuplas.ts
@@ -0,0 +1,26 @@
+let tupla1: [string, boolean]
+
+tupla1 = ['hola', true]
+
+// las siguientes asignaciones nos dan error
+// poor que una tupla typescript la considera fuertemente
+//tupla1 = ['holña']
+//tupla1 = ['holña',  false, 'hola']
+//tupla1 = [false, 'holña']
+
+let [tuplaString, tuplaBoolean] = tupla1
+
+console.log(tuplaString)
+console.log(tuplaBoolean)
+
+let complexObj = {
+    aNum: 1,
+    bStr: 'name',
+    cBool: true
+}
+
+let { aNum, bStr, cBool} = complexObj
+
+console.log(aNum)
+console.log(bStr)
+console.log(cBool)
