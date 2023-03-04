a2367a58767426887fa39b66f51883290083c4a1
joserubio16 <jose.rubio16@hotmail.com>
2023-03-04 10:33:42 -0600

    clases/ declaracion e instancias

---
+++ src/session-4/clases.ts
@@ -0,0 +1,9 @@
+class SimpleClass{
+    id: number | undefined;
+    print(): void{
+        console.log('SimpleClass.print() ha sido llamado')
+    }
+}
+
+let mySimpleClass = new SimpleClass();
+mySimpleClass.print()
