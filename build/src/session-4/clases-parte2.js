9d33dc022155540e71b9d2a494bbbcba9db0ed9f
joserubio16 <jose.rubio16@hotmail.com>
2023-03-04 12:04:47 -0600

    clases/ private

--- build/src/session-4/clases-parte2.js
+++ build/src/session-4/clases-parte2.js
@@ -6,3 +6,23 @@ class ClassWithConstructor {
 }
 let classWC = new ClassWithConstructor(10);
 console.log(`classwc = ${JSON.stringify(classWC)}`);
+//clases
+//clase publica
+class ClassWithPublicProperty {
+}
+let myClassWithPublicP = new ClassWithPublicProperty();
+myClassWithPublicP.id = 10;
+//clase privada
+class ClaseConPropiedadPrivada {
+    constructor(id) {
+        this.id = id;
+    }
+    print() {
+        console.log(this.id);
+    }
+}
+let miClasePrivada = new ClaseConPropiedadPrivada(11);
+// nno se puede acceder a una propiuedad privada fuera de la clase, pero si uilizar metodos
+// publicos para acceder a ellos
+//miClasePrivada.id = 10
+miClasePrivada.print();
--- src/session-4/clases-parte2.ts
+++ src/session-4/clases-parte2.ts
@@ -8,3 +8,31 @@ class ClassWithConstructor {
 let classWC = new ClassWithConstructor(10)
 
 console.log(`classwc = ${JSON.stringify(classWC)}`)
+
+//clases
+//clase publica
+class ClassWithPublicProperty{
+   public id: number | undefined
+}
+
+let myClassWithPublicP = new ClassWithPublicProperty();
+myClassWithPublicP.id = 10
+
+
+//clase privada
+
+class ClaseConPropiedadPrivada {
+    private id: number | undefined
+    constructor(id: number) {
+        this.id = id
+    }
+    print(){
+        console.log(this.id)
+    }
+}
+
+let miClasePrivada = new ClaseConPropiedadPrivada(11);
+// nno se puede acceder a una propiuedad privada fuera de la clase, pero si uilizar metodos
+// publicos para acceder a ellos
+//miClasePrivada.id = 10
+miClasePrivada.print() 
