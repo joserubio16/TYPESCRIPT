dce94109d6e3ec19b07e4f0f131cbc7f6579b0b3
joserubio16 <jose.rubio16@hotmail.com>
2023-03-04 10:52:28 -0600

    clases/ implementando interfaces

--- build/src/session-4/clases.js
+++ build/src/session-4/clases.js
@@ -10,3 +10,21 @@ mySimpleClass.print();
 let mySimpleClassA = new SimpleClass();
 mySimpleClassA.id = 30;
 mySimpleClassA.print();
+//implementacvion de interfaces
+class ClassA {
+    print() {
+        console.log('ClassA.print() ha sido llamado');
+    }
+}
+class ClassB {
+    print() {
+        console.log('ClassB.print() ha sido llamado');
+    }
+}
+function printClass(a) {
+    a.print();
+}
+let myClassA = new ClassA();
+let myClassB = new ClassB();
+printClass(myClassA);
+printClass(myClassB);
--- src/session-4/clases.ts
+++ src/session-4/clases.ts
@@ -12,3 +12,31 @@ mySimpleClass.print()
 let mySimpleClassA = new SimpleClass();
 mySimpleClassA.id = 30
 mySimpleClassA.print()
+
+
+//implementacvion de interfaces
+class ClassA implements Print {
+    print(){
+        console.log('ClassA.print() ha sido llamado')
+    }
+}
+
+class ClassB implements Print{
+    print(){
+        console.log('ClassB.print() ha sido llamado')
+    }
+}
+
+interface Print{
+    print(): void;
+}
+
+function printClass(a: Print){
+    a.print()
+}
+
+let myClassA = new ClassA()
+let myClassB = new ClassB()
+
+printClass(myClassA)
+printClass(myClassB)
