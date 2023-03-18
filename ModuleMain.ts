1e6675676ceb6a213eddd6b944aeafa3b55ef0c0
joserubio16 <jose.rubio16@hotmail.com>
2023-03-18 10:06:46 -0600

    default export

--- ModuleMain.ts
+++ ModuleMain.ts
@@ -6,6 +6,9 @@ import { Module1 as MyMod1 } from "./src/session-2/Module";
 //exportar multiplaces clases
 import { MultipleClass1, MultipleClass2 } from "./src/modules/MultipleExports";
 
+//mod default
+import DefaultAdd from "./src/modules/DefaultExport";
+
 let myModule1 = new Module1()
 myModule1.print()
 
@@ -15,3 +18,5 @@ myMod1.print()
 let myMC1 = new MultipleClass1
 let myMC2 = new MultipleClass2
 
+let modDefault = DefaultAdd(1, 2) 
+console.log(modDefault)
--- build/ModuleMain.js
+++ build/ModuleMain.js
@@ -1,4 +1,7 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Module1_1 = require("./src/modules/Module1");
 // RENOMBRAR modulos, nos sirve para cuando se repiten los nombres
@@ -6,9 +9,13 @@ const Module1_1 = require("./src/modules/Module1");
 const Module_1 = require("./src/session-2/Module");
 //exportar multiplaces clases
 const MultipleExports_1 = require("./src/modules/MultipleExports");
+//mod default
+const DefaultExport_1 = __importDefault(require("./src/modules/DefaultExport"));
 let myModule1 = new Module1_1.Module1();
 myModule1.print();
 let myMod1 = new Module_1.Module1();
 myMod1.print();
 let myMC1 = new MultipleExports_1.MultipleClass1;
 let myMC2 = new MultipleExports_1.MultipleClass2;
+let modDefault = (0, DefaultExport_1.default)(1, 2);
+console.log(modDefault);
